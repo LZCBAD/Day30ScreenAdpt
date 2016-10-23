97bc05b256757b82a0ab7b60dc0a4b29116bc09f
LZCBAD <janzenlee@163.com>
2016-10-23 16:48:54 +0800

    测试提交

--- app/src/main/java/com/qf/xs/day30screenadpt/MainActivity.java
+++ app/src/main/java/com/qf/xs/day30screenadpt/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
-
+                //测试提交代码
     }
 
 
